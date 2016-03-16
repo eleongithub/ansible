4faa755907281c4e956ee9efcbda3c04116145de
eleongithub <elegithub@gmail.com>
2016-03-16 13:21:52 +0100

    Improvment

--- step-02/run.sh
+++ step-02/run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ansible-playbook -i inventory/hosts main.yml --ask-su-pass -vvvv
+ssh_id_dsa_key_file=~/.ssh/id_dsa
+passphrase="ansible"
+# Demarrage d'un agent ssh
+eval $(ssh-agent)
+expect <<EOF
+spawn ssh-add ${ssh_id_dsa_key_file}
+expect "Enter passphrase for ${ssh_id_dsa_key_file}:"
+send "${passphrase}\n"
+expect eof
+exit
+EOF
+ansible-playbook -i inventory/hosts main.yml -vvvv
