52396c6ee793e292f81a7df75caab6446395e364
eleongithub <elegithub@gmail.com>
2016-03-18 22:23:05 +0100

    Improvment of jdk roles

--- step-00/run.sh
+++ step-00/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+ssh_auth_file=~/ansible/ssh_authentification.sh
+source $ssh_auth_file
 ansible-playbook -i hosts main.yml -vvvv
--- step-02/run.sh
+++ step-02/run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-ssh_id_dsa_key_file=~/.ssh/id_dsa
-passphrase="ansible"
-# Demarrage d'un agent ssh
-eval $(ssh-agent)
-expect <<EOF
-spawn ssh-add ${ssh_id_dsa_key_file}
-expect "Enter passphrase for ${ssh_id_dsa_key_file}:"
-send "${passphrase}\n"
-expect eof
-exit
-EOF
+ssh_auth_file=~/ansible/ssh_authentification.sh
+source $ssh_auth_file
 ansible-playbook -i inventory/hosts main.yml -vvvv 
