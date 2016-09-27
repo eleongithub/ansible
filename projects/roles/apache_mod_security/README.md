f4afa85ba98bf0f27c5bf2494135bad1f19aed6c
eleongithub <elegithub@gmail.com>
2016-09-27 16:50:41 +0200

    Usage Ansible-Vault. Improve codes

---
+++ projects/roles/apache_mod_security/README.md
@@ -0,0 +1,5 @@
+	      Ansible Step-02 : Apache Role
+
+	    This role install Apache Web Server.
+
+    Download Apache HTTP tarball file on http://httpd.apache.org/download.cgi
--- projects/run.sh
+++ projects/run.sh
@@ -9,4 +9,8 @@ ansible --version
 
 echo "---------------------"
 
-ansible-playbook -i inventory/dev/hosts.yml main.yml -vvvv 
+# TODO : Get this variable from Jenkins password input 
+VAULT_PASSWORD="test"
+echo $VAULT_PASSWORD > vault_pass.txt
+ansible-playbook --vault-password-file vault_pass.txt -i inventory/dev/hosts.yml main.yml -vvv
+rm vault_pass.txt
