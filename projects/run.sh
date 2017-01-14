3da242ed3ae6c1f443bc7aa2733928d9b27c60f9
eleongithub <elegithub@gmail.com>
2017-01-14 15:18:30 +0100

    Deplacement des variables  dans les repertoires default

--- projects/run.sh
+++ projects/run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 ssh_auth_file=ssh_authentification.sh
-
 source $ssh_auth_file
-
 echo "-- Ansible version --"
-
 ansible --version
-
 echo "---------------------"
-
-# TODO : Get this variable from Jenkins input password  
+# TODO : VAULT_PASSWORD & env variables will be get from Jenkins input data  
 VAULT_PASSWORD="test"
 echo $VAULT_PASSWORD > vault_pass.txt
 ansible-playbook --vault-password-file vault_pass.txt -i inventory/dev/hosts.yml main.yml -vvv -e "env=dev"
